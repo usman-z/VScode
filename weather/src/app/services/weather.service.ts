7df1cbab47da0791f47f731f0dd76f329cb2f81e
Admin <usmanzia366@gmail.com>
2023-05-16 17:05:53 -0400

    Done for the Day

---
+++ weather/src/app/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getWeatherData(cityName: string) {
+    
+  }
+
+
+}
