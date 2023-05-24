ec659cacc55e7b745358c1307abfdd338e2b88ba
Admin <usmanzia366@gmail.com>
2023-05-24 12:24:53 -0400

    Finished

--- weather/src/app/app.component.ts
+++ weather/src/app/app.component.ts
@@ -28,7 +28,6 @@ export class AppComponent implements OnInit{
     this.weatherService.getWeatherData(cityName).subscribe({
       next: (response) => {
         this.weatherData = response;
-        console.log(response);
       }
     });
   }
--- weather/src/app/models/weather.model.ts
+++ weather/src/app/models/weather.model.ts
@@ -1,29 +1,28 @@
 export interface WeatherData {
-     location: Location
-     current: Current
-   }
-   
-   export interface Location {
-     name: string
-     region: string
-     country: string
-     lat: number
-     lon: number
-     tz_id: string
-     localtime_epoch: number
-     localtime: string
-   }
-   
-   export interface Current {
-     temp_c: number
-     temp_f: number
-     is_day: number
-     condition: Condition
-     wind_mph: number
-     humidity: number
-     feelslike_f: number
-     uv: number
-   }
-   
-   export interface Condition {}
-   
+  location: Location
+  current: Current
+}
+
+export interface Location {
+  name: string
+  region: string
+  country: string
+  lat: number
+  lon: number
+  tz_id: string
+  localtime_epoch: number
+  localtime: string
+}
+
+export interface Current {
+  temp_c: number
+  temp_f: number
+  is_day: number
+  condition: Condition
+  wind_mph: number
+  humidity: number
+  feelslike_f: number
+  uv: number
+}
+
+export interface Condition {}
